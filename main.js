9b108d85d41a5de829aebb83b8c5ca8e38323bd6
Pavithra <pavithrasekar246@gmail.com>
2025-01-20 16:18:17 +0530

    first point

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed= new Typed(".text", {
+    strings: ["Front End Developer", "Web Developer", "Python Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    })
+
+    // project start
+
+    document.querySelector('.project-link').addEventListener('click', function() {
+        alert('You are about to view the project!');
+    });
+
+    // project end
